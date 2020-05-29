e8682cca6ce1ee0bbabbfde0bc3164320030c97f
chura414 <sugiyama_8064@yahoo.co.jp>
2020-05-29 18:51:30 +0900

    controller

--- webApp/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-	validates :name, {presence: true, uniqueness: true}
-	validates :email, {presence: true}
-	validates :password, {presence: true}
-end
--- webApp/db/schema.rb
+++ webApp/db/schema.rb
@@ -10,24 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_131407) do
+ActiveRecord::Schema.define(version: 2020_05_29_095049) do
 
-  create_table "diaries", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "records", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
   end
 
 end
