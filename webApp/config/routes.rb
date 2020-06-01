e4dc9ae4aedb1d3b20da6b0efc650dc23eb5f904
chura414 <sugiyama_8064@yahoo.co.jp>
2020-06-01 10:33:02 +0900

    for deploy

--- webApp/config/routes.rb
+++ webApp/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   get 'users/index'
   get 'posts/index'
-  get 'home/top'
+  get '/' => 'home#top'
+
+	root 'home#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
